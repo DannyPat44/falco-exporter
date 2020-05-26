d760c3a41d7d2fbd188e0c14ca8f888576cf5d9b
Leonardo Grasso <me@leonardograsso.com>
2020-05-26 12:16:36 +0200

    update(pkg/exporter): split receiving from subscription
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- pkg/exporter/exporter.go
+++ pkg/exporter/exporter.go
@@ -6,7 +6,11 @@ import (
 
 	"github.com/falcosecurity/client-go/pkg/api/output"
 	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/grpc"
 )
+
+// A RecvFunc waits for subscribed events and forwards to metric counters.
+type RecvFunc func() error
 
 var (
 	eventsCounter = prometheus.NewCounterVec(
@@ -25,25 +29,35 @@ func init() {
 	prometheus.MustRegister(eventsCounter)
 }
 
-// Subscribe to a ServiceClient to receive a stream of falco output events.
-func Subscribe(ctx context.Context, outputClient output.ServiceClient) error {
+// Subscribe to a ServiceClient to receive a stream of Falco output events.
+// If success, it returns a RecvFunc that can be used.
+// To stop the subscription, use Cancel() on the context provided.
+func Subscribe(ctx context.Context, outputClient output.ServiceClient, opts ...grpc.CallOption) (RecvFunc, error) {
+
 	// Keepalive true means that the client will wait indefinitely for new events to come
-	// Use keepalive false if you only want to receive the accumulated events and stop
-	fcs, err := outputClient.Subscribe(ctx, &output.Request{Keepalive: true})
+	fcs, err := outputClient.Subscribe(ctx, &output.Request{Keepalive: true}, opts...)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	for {
-		res, err := fcs.Recv()
-		if err != nil {
-			return err
+	return func() error {
+		for {
+			res, err := fcs.Recv()
+			if err != nil {
+				return err
+			}
+
+			eventsCounter.With(prometheus.Labels{
+				"rule":     res.Rule,
+				"priority": fmt.Sprintf("%d", res.Priority),
+				"hostname": res.Hostname,
+			}).Inc()
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+			}
 		}
-
-		eventsCounter.With(prometheus.Labels{
-			"rule":     res.Rule,
-			"priority": fmt.Sprintf("%d", res.Priority),
-			"hostname": res.Hostname,
-		}).Inc()
-	}
+	}, nil
 }
